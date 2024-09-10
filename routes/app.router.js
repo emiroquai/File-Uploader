69325f29b6b86d61a4c656a80ccb7e72aaae2f01
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-09-10 12:43:05 +0200

    Multer uses original filename

--- routes/app.router.js
+++ routes/app.router.js
@@ -3,7 +3,15 @@ const router = Router();
 const controller = require("../controllers/app.controller");
 
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
 router.get("/", controller.getHome);
 router.get("/sign-up", controller.getSignUp);
